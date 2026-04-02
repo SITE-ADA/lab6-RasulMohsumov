fd938b217ce90a60e5ff9ac97d19268c4ad5337c
RasulMohsumov <rmohsumov17860@ada.edu.az>
2026-04-02 13:42:14 +0400

    Add ProductRepository

--- src/main/java/az/edu/ada/wm2/lab6/repository/ProductRepository.java
+++ src/main/java/az/edu/ada/wm2/lab6/repository/ProductRepository.java
@@ -1,11 +1,13 @@
 package az.edu.ada.wm2.lab6.repository;
 
 import az.edu.ada.wm2.lab6.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface ProductRepository {
+public interface ProductRepository extends JpaRepository<Product, UUID> {
     Product save(Product product);
     Optional<Product> findById(UUID id);
     List<Product> findAll();
