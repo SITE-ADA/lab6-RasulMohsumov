674ab53297f09d5f5388d471f49a9b6a2b796ade
RasulMohsumov <rmohsumov17860@ada.edu.az>
2026-04-02 22:43:20 +0400

    repository imports

--- src/main/java/az/edu/ada/wm2/lab6/repository/ProductRepository.java
+++ src/main/java/az/edu/ada/wm2/lab6/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package az.edu.ada.wm2.lab6.repository;
 
 import az.edu.ada.wm2.lab6.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 import java.util.List;
 import java.util.Optional;
